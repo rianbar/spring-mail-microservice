08a5dc6fda988a68ec721775043b344563049374
rian <rianbarroso596@gmail.com>
2023-07-31 09:14:21 -0300

    Authorities fixed

--- products/src/main/java/com/ms/products/configs/SecurityAppConfig.java
+++ products/src/main/java/com/ms/products/configs/SecurityAppConfig.java
@@ -1,22 +1,16 @@
 package com.ms.products.configs;
 
-import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -39,7 +33,7 @@ public class SecurityAppConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorization -> authorization
                         .requestMatchers(HttpMethod.GET, PATTERN_ROUTE)
-                        .hasRole(USER_ROLE).anyRequest().hasRole(ADMIN_ROLE))
+                        .hasAuthority(USER_ROLE).anyRequest().hasAuthority(ADMIN_ROLE))
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
