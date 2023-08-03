5e0b3e7bb65ddd87c5b6404cb60a8f4076b75c79
rian <rianbarroso596@gmail.com>
2023-08-03 14:31:03 -0300

    rate limit implemented

--- products/src/main/java/com/ms/products/ProductsApplication.java
+++ products/src/main/java/com/ms/products/ProductsApplication.java
@@ -3,8 +3,10 @@ package com.ms.products;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ProductsApplication {
 
 	public static void main(String[] args) {
